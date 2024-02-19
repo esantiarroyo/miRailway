279af3d3e1a5c140081d7e62a6f13cc9d26093f8
SANTIAGO <santimal1153@gmail.com>
2024-02-19 09:12:00 -0500

    comboBox en los nuevos usuarios y en los modificar

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 # config.py
 
 class Config:
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:E2GE-4d-C5a-45BGgfeDdC4gDEb6-Bb2@viaduct.proxy.rlwy.net:49674/proyectofinal'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:a5ba4g6C1bBG2Gec6dBBcHg6bCgbDF44@monorail.proxy.rlwy.net:16262/proyectoSanti'
     
     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@127.0.0.1:3306/proyectofinal'
